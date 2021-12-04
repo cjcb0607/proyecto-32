4c1486c7a191bbf1b7b96ee8f2e32a1cee51144b
cjcb0607 <91701797+cjcb0607@users.noreply.github.com>
2021-12-04 13:50:20 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Events = Matter.Events,
+    Bodies = Matter.Bodies; 
+var balls = [];
+var plinkos = [];
+var divisions =[];
+var ball;
+
+var divisionHeight=300;
+var score =0;
+var count= 0;
+var PLAY= 1;
+var  END=0;
+var gameState = PLAY;
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+    for (var j = 75; j <=width; j=j+50) {
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,175));
+    }
+
+    for (var j = 75; j <=width; j=j+50) {
+        plinkos.push(new Plinko(j,275));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,375));
+    }
+    
+}
+ 
+function draw() {
+  background("black");
+  textSize(35)
+  text("Puntuación : "+score,20,40);
+  fill("white");
+
+  textSize(35)
+  text(" 500 ", 5, 550);
+  text(" 500 ", 80, 550);
+  text(" 500 ", 160, 550);
+  text(" 500 ", 240, 550);
+  text(" 100 ", 320, 550);
+  text(" 100 ", 400, 550);
+  text(" 100 ", 480, 550);
+  text(" 250 ", 560, 550);
+  text(" 250 ", 640, 550);
+  text(" 250 ", 720, 550);
+  Engine.update(engine);
+  ground.display();
+
+
+  for (var i = 0; i < plinkos.length; i++) {
+     plinkos[i].display();  
+  }
+ 
+    if(ball!=null)
+    {
+       ball.display();
+       if (ball.body.position.y>760){
+          if (ball.body.position.x<300 ){
+            score= score+500;
+            ball= null;
+        }
+          if (ball.body.position.x>601 && ball.body.position.x<900){
+           score= score+250;
+          ball= null;
+       }
+        if (ball.body.position.x>301 && ball.body.position.x<600){
+          score=score+100;
+          ball= null;
+       }
+       }
+       
+    }
+
+   for (var k = 0; k < divisions.length; k++) 
+   {
+     divisions[k].display();
+   }
+   
+   if (count===6){
+     gameState= END;
+   }
+
+
+   if (gameState === END ){
+     textSize(100);
+     text("GameOver",150,250);
+   }
+ 
+}
+
+
+function mousePressed()
+{
+  if(gameState!== END){
+    count++;
+    ball=new Ball(mouseX, 10, 10, 10);  
+  }
+  
+}
+
